5dc1fd49cd711907d798866a8a01645eec04639e
GermainMichaud <gmichaud.dev@gmail.com>
2021-11-05 01:41:39 +0100

    :art: Update todo form component

--- src/app/todos/todo-form/todo-form.component.ts
+++ src/app/todos/todo-form/todo-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.scss']
+  styleUrls: ['./todo-form.component.scss'],
 })
 export class TodoFormComponent implements OnInit {
+  public addTodoForm: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.addTodoForm = this.fb.group({
+      new_todo: ['', [Validators.required, Validators.minLength(3)]],
+    });
   }
 
+  public addTodo(event: Event): void {
+    event.preventDefault();
+    console.log(this.addTodoForm.value);
+    this.todoService.addTodo(this.addTodoForm.value.new_todo);
+    this.addTodoForm.reset();
+  }
 }
