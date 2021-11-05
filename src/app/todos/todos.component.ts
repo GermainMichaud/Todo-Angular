95c86ae131531c119f2c2aa6b5dcd00a9c3f0fd7
GermainMichaud <gmichaud.dev@gmail.com>
2021-11-05 01:40:41 +0100

    :art: Update todos component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
+  constructor(public todoService: TodoService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
