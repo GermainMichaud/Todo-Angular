8e431ce0040999322dc5af9a801b4e0c2b3fcbbb
GermainMichaud <gmichaud.dev@gmail.com>
2021-11-05 01:55:04 +0100

    :sparkles: Update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { TodosComponent } from './todos/todos.component';
 
 const routes: Routes = [
   {
     path: '',
     component: TodosComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
