da11707cd47a69eaca03cde4b1c381a59474c854
GermainMichaud <gmichaud.dev@gmail.com>
2021-11-05 02:06:19 +0100

    :sparkles: Import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { TodoFormComponent } from './todos/todo-form/todo-form.component';
 import { TodoItemComponent } from './todos/todo-item/todo-item.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -17,7 +18,12 @@ import { LoginComponent } from './login/login.component';
     TodoItemComponent,
     LoginComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
