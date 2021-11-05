e374104622ddcf355fdd68190d9802e45e36e647
GermainMichaud <gmichaud.dev@gmail.com>
2021-11-05 03:05:01 +0100

    :zap: Add guard to protected route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { TodosComponent } from './todos/todos.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   {
     path: '',
     component: TodosComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'login',
