c463a4700f571af5a98a1b8027ea3937feb05373
GermainMichaud <gmichaud.dev@gmail.com>
2021-11-05 03:02:21 +0100

    :sparkles: Create interceptor

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      tap((event) => {
+        if (event instanceof HttpResponse) {
+          if (event.url?.endsWith('/auth/login') && event.status === 200) {
+            const body = event.body as {
+              token: string;
+              user: { id: number; name: string };
+            };
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('user', JSON.stringify(body.user));
+            this.authService.setUser(body.user);
+            this.router.navigate(['/']);
+          } else if (event.status === 401) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            this.authService.setUser(null);
+            this.router.navigate(['/login']);
+          }
+        }
+      })
+    );
+  }
+}
